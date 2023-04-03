cefd3e4a05ac9eda3d95b6013a502d8a54a58a5f
Admin <admin.push@hackerrank.workspace>
2023-04-03 17:21:22 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var storage = []
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('<p>HTML Data</p>');
 });
+
+route.get('/products',function(req,res,next){
+  var json = {
+    "id":storage.length+1,
+    "name":req.name,
+    "price":req.price,
+    "mrp":req.mrp,
+    "stock":req.stock,
+    "isPublished":req.isPublished
+  };
+  storage.push(json);
+  res.send(storage);
+})
 
 module.exports = router;
